f059e60078aca1c8fa5cf7905c9fd3d71f3f3b05
Mateo <mateodioev@noemail.com>
2022-09-27 12:55:55 -0500

    fake stdClass

---
+++ src/fakeStdClass.php
@@ -0,0 +1,52 @@
+<?php 
+
+namespace Mateodioev\Utils;
+
+use stdClass;
+
+class fakeStdClass
+{
+  protected stdClass $std;
+  protected mixed $defaultReturnValue;
+
+  public function __construct(?stdClass $std = null) {
+    if ($std !== null) {
+      $this->std = $std;
+    } else {
+      $this->std = new stdClass;
+    }
+
+    $this->defaultReturnValue = '';
+  }
+
+  public function setReturnValue(mixed $value): fakeStdClass
+  {
+    $this->defaultReturnValue = $value;
+    return $this;
+  }
+
+  public function getIntance(): fakeStdClass
+  {
+    return $this;
+  }
+  
+  public function __set($name, $value)
+  {
+    $this->std->$name = $value;
+  }
+
+  public function __get($name)
+  {
+    return $this->std->$name ?? $this->defaultReturnValue;
+  }
+
+  public function __toString()
+  {
+    return json_encode($this->std);
+  }
+
+  public function __debugInfo()
+  {
+    return (array) $this->std;
+  }
+}
