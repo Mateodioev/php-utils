eeef9d2678b12adcb9c5cdff5183988c93ca5e30
Mateo <orehurcomito@gmail.com>
2023-01-05 23:36:14 -0500

    add custom startTime

--- src/Time.php
+++ src/Time.php
@@ -7,12 +7,13 @@ class Time
     /**
      * Calculate the execution time of a script
      *
-     * @param boolean $float true to return a float, false to return an int
-     * @param integer $precision number of decimal places to return
+     * @param bool $float true to return a float, false to return an int
+     * @param bool $precision number of decimal places to return
+	 * @param float|int $startTime The time the script started, pass null to use the default
      */
-    public static function Took(bool $float = false, int $precision = 4): float|int
+    public static function Took(bool $float = false, int $precision = 4, float|int $startTime = null): float|int
     {
-        $start = $_SERVER['REQUEST_TIME_FLOAT'] ?? $_SERVER['REQUEST_TIME'];
+        $start = $startTime ?? $_SERVER['REQUEST_TIME_FLOAT'] ?? $_SERVER['REQUEST_TIME'];
         $end = microtime(true);
         $took = $end - $start;
         return $float ? (float) round($took, $precision) : (int) $took;
